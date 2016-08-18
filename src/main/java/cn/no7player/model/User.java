4f3152d7859567d9fb82b7ac77d9ba450e5c5afd
lifeng <18600161244@163.com>
2016-08-18 17:26:48 +0800

    springboot mybatis demo

---
+++ src/main/java/cn/no7player/model/User.java
@@ -0,0 +1,21 @@
+package cn.no7player.model;
+
+/**
+ * Created by zl on 2015/8/27.
+ */
+public class User {
+    private String username;
+    private String realname;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+}
